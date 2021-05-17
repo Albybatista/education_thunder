ac5bb60290add151245ba1a20f244f74f28a9068
Joshua <millerjoshua737@gmail.com>
2021-05-17 14:40:53 -0500

    React router set up for the main and intro page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
 import MainPage from './components/MainPage/MainPage';
+import IntroPage from './components/IntroPage/IntroPage'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+const routes = [
+  {
+    Component: MainPage,
+    key: 'MainPage',
+    path: '/main'
+  },
+  {
+    Component: IntroPage,
+    key: 'IntroPage',
+    path: '/'
+  }
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <MainPage />
-    </div>
+    <Router>
+      <Switch>
+        {
+          routes.map(({Component, key, path}) => {
+            return(
+              <Route 
+                  key={key}
+                  path={path}
+                  component={() => <Component page={key} />}
+              />
+            )
+          })
+        }
+      </Switch>
+    </Router>
   );
 }
 
