2523e89e000d544464e317c5eaf47dfe63232944
Joshua <millerjoshua737@gmail.com>
2021-05-18 11:10:44 -0500

    useContext hook implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import React, {useState} from 'react';
 import MainPage from './components/MainPage/MainPage';
 import IntroPage from './components/IntroPage/IntroPage'
 import CategoryPage from './components/CategoryPage/CategoryPage'
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import TheContext from './context/index';
+
 
 const routes = [
   {
@@ -24,22 +27,31 @@ const routes = [
 
 
 function App() {
+  const [categoryNum, setCategoryNum] = useState()
+  
+
+
   return (
-    <Router>
-      <Switch>
-        {
-          routes.map(({Component, key, path}) => {
-            return(
-              <Route 
-                  key={key}
-                  path={path}
-                  component={() => <Component page={key} />}
-              />
-            )
-          })
-        }
-      </Switch>
-    </Router>
+    <TheContext.Provider value={{
+      categoryNum: categoryNum,
+      setCategoryNum: setCategoryNum
+    }}>
+      <Router>
+        <Switch>
+          {
+            routes.map(({Component, key, path}) => {
+              return(
+                <Route 
+                    key={key}
+                    path={path}
+                    component={() => <Component page={key} />}
+                />
+              )
+            })
+          }
+        </Switch>
+      </Router>
+    </TheContext.Provider>
   );
 }
 
